31fdbb7acde341128b777b43ca9cd836c34d68b6
shehan82 <shehansandeepa82@gmail.com>
2020-11-07 15:21:47 +0530

    navIcon changed

--- web/resources/javascript/side_navbar.js
+++ web/resources/javascript/side_navbar.js
@@ -1,6 +1,7 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  
 
 }
 
