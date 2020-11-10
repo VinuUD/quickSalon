0674e3b9816e6d7ed98aebe2a3da1a721625ec19
shehan82 <shehansandeepa82@gmail.com>
2020-11-10 22:01:04 +0530

    added register Service page

---
+++ web/resources/javascript/registerService.js
@@ -0,0 +1,39 @@
+
+function add()
+{
+    var tr = document.createElement("tr");
+    tr.setAttribute("class", "tr");
+
+    var select = document.createElement("select");
+    select.setAttribute("class","blo"); //should be there
+    select.setAttribute("id","sel");
+    
+    var option = document.createElement("option");
+    option.setAttribute("class","option");
+    option.innerHTML = "waruna";
+
+    var option1 = document.createElement("option");
+    option1.setAttribute("id","option");
+    option1.innerHTML = "praba";
+
+    select.appendChild(option);
+    select.appendChild(option1);
+    tr.appendChild(select)
+
+
+    document.getElementById("add").appendChild(tr);
+
+    document.getElementById("sel").addEventListener("click", function ()
+    {
+        tr.innerHTML = "<td>" + document.getElementById("sel").value + "</td>";
+    });
+
+
+   
+}
+
+
+
+
+
+
