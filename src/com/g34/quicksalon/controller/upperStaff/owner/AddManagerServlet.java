f33a8916e17e94c34033e3468f24350ba4d04431
shehan82 <shehansandeepa82@gmail.com>
2020-11-17 19:08:04 +0530

    added addManageServlet.java

---
+++ src/com/g34/quicksalon/controller/upperStaff/owner/AddManagerServlet.java
@@ -0,0 +1,48 @@
+package com.g34.quicksalon.controller.upperStaff.owner;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
+
+import com.g34.quicksalon.entity.ManagerDetails;
+import com.g34.quicksalon.model.AddManagerModel;
+
+public class AddManagerServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException
+	{
+		LocalDate obj = LocalDate.now();
+		
+		
+		String nic = req.getParameter("nic");
+		String firstName = req.getParameter("firstName");
+		String lastName = req.getParameter("lastName");
+		int salary = Integer.parseInt(req.getParameter("salary"));
+		String enrollDate = obj.toString();
+		String resignDate = "0000-00-00";
+		int upFlag = 1;
+		int leaveFlag = 0;
+		int removeFlag = 0;
+		int cNum = Integer.parseInt(req.getParameter("cNum"));
+		
+		
+		
+		
+		
+		ManagerDetails Manager = new ManagerDetails(nic, firstName, lastName, salary, enrollDate, resignDate, upFlag, leaveFlag, removeFlag, cNum);
+		
+		AddManagerModel model = new AddManagerModel();
+		
+		try {
+			model.addManager(Manager);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
