749da50a3b3dad39d1805305d54520201a2b0a31
Shashira <shashiradw7@gmail.com>
2020-11-22 14:47:21 +0530

    ManagerDAO interface created

--- src/com/g34/quicksalon/dao/ManagerDAO.java
+++ src/com/g34/quicksalon/dao/ManagerDAO.java
@@ -1,2 +1,8 @@
-package com.g34.quicksalon.dao;public interface ManagerDAO {
+package com.g34.quicksalon.dao;
+
+import com.g34.quicksalon.model.ManagerDetails;
+
+public interface ManagerDAO {
+
+    public void addManager(ManagerDetails manager) throws Exception;
 }
