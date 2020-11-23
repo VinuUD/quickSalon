26171bccf269bd821f6cf48af139bc578f65653f
Shashira <shashiradw7@gmail.com>
2020-11-23 14:25:23 +0530

    Register customer servlet added

--- src/com/g34/quicksalon/controller/RegisterCustomerServlet.java
+++ src/com/g34/quicksalon/controller/RegisterCustomerServlet.java
@@ -6,14 +6,55 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class RegisterCustomerServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+//         String fname=request.getParameter("fname");
+//         lname
+//         uname
+//         contactno
+//         nic
+//         email
+//         address
+//         password
+//         password-repeat
+
+        String fname=request.getParameter("fname");
+        String lname=request.getParameter("lname");
+        String uname=request.getParameter("uname");
+        String contactno=request.getParameter("contactno");
+        String nic=request.getParameter("nic");
+        String email=request.getParameter("email");
+        String address=request.getParameter("address");
+        String password=doHash(request.getParameter("password"));
+
+
+
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
+
+    public String doHash(String password){
+        try {
+            MessageDigest messageDigest=MessageDigest.getInstance("SHA-256");
+            messageDigest.update(password.getBytes());
+            byte[] hashedByte=messageDigest.digest();
+            StringBuilder sb=new StringBuilder();
+            for(byte b:hashedByte){
+                sb.append(String.format("%02x",b));
+            }
+            return  sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
