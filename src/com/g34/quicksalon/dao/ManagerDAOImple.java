71bd8f53cd1f427dd1612be7603adaf9224535ae
shehan82 <shehansandeepa82@gmail.com>
2020-11-24 12:55:34 +0530

    change the db query

--- src/com/g34/quicksalon/dao/ManagerDAOImple.java
+++ src/com/g34/quicksalon/dao/ManagerDAOImple.java
@@ -63,16 +63,14 @@ public class ManagerDAOImple implements ManagerDAO {
 
     public ArrayList<ManagerDetailsForView> getManagersDetails() throws Exception
     {
-        Class.forName("com.mysql.jdbc.Driver");
-
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quick_salon","root","");
+		Connection	con = DBConnection.getConnection();
         Statement st = con.createStatement();
 		
-		ResultSet rs = st.executeQuery("select j4f9qe_employee.employeeID, j4f9qe_employee.firstName, j4f9qe_employee.lastName, j4f9qe_employeecontacts.contactNum, j4f9qe_employee.nicNo, j4f9qe_employee.salary from j4f9qe_employee INNER JOIN j4f9qe_employeecontacts ON j4f9qe_employee.employeeID = j4f9qe_employeecontacts.empID WHERE j4f9qe_employee.isUpperStaffFlag = 1");
+		ResultSet rs = st.executeQuery("select j4f9qe_employee.employeeID, j4f9qe_employee.firstName, j4f9qe_employee.lastName, j4f9qe_employeecontacts.contactNum, j4f9qe_employee.nicNo, j4f9qe_employee.salary, j4f9qe_employee.email, j4f9qe_employee.address from j4f9qe_employee INNER JOIN j4f9qe_employeecontacts ON j4f9qe_employee.employeeID = j4f9qe_employeecontacts.empID WHERE j4f9qe_employee.isUpperStaffFlag = 1");
 	   
 		while(rs.next())
         {
-            AllManagersDetails.add(new ManagerDetailsForView(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4),rs.getString(5), rs.getInt(6)));
+            AllManagersDetails.add(new ManagerDetailsForView(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getInt(4),rs.getString(5), rs.getInt(6), rs.getString(7), rs.getString(8)));
 
 		}
 		
