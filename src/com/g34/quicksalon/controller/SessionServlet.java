6365dcfb2afdd2efb6fa3fc8cdd93024bdfad812
Shashira <shashiradw7@gmail.com>
2020-11-25 10:59:07 +0530

    login session displyed

---
+++ src/com/g34/quicksalon/controller/SessionServlet.java
@@ -0,0 +1,23 @@
+package com.g34.quicksalon.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class SessionServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out= response.getWriter();
+        HttpSession session= request.getSession();
+        String userName= String.valueOf(session.getAttribute("username"));
+        out.println(userName);
+    }
+}
