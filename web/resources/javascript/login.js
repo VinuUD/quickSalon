12818150177930fc14cd055727b0570f0dfd69fe
Shashira <shashiradw7@gmail.com>
2021-02-06 13:47:29 +0530

    Login validation fixed

--- web/resources/javascript/login.js
+++ web/resources/javascript/login.js
@@ -1,19 +1,17 @@
-
 (function($) {
   "use strict";
 
   var input = $('.validate-input .input34');
-
+  
   $('.validate-form').on('submit', function(){
     var check = true;
-
+    
     for(var i = 0; i < input.length; i++){
-      if(validate(input[i] == false)){
+      if(validate(input[i]) == false){
         showValidate(input[i]);
         check = false;
       }
     }
-
     return check;
   });
 
@@ -26,7 +24,7 @@
   function validate (input) {
     if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
       if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-          return false;
+        return false;
       }
     }
     else {
