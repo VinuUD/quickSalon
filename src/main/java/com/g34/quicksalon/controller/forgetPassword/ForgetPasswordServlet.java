e477e8938c4150672e87bf602953b27b40c1deea
Shashira <shashiradw7@gmail.com>
2021-02-08 16:28:10 +0530

    Forget password servlet created

--- src/main/java/com/g34/quicksalon/controller/forgetPassword/ForgetPasswordServlet.java
+++ src/main/java/com/g34/quicksalon/controller/forgetPassword/ForgetPasswordServlet.java
@@ -1,9 +1,17 @@
-public class ForgetPasswordServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+package com.g34.quicksalon.controller.forgetPassword;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ForgetPasswordServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 }
