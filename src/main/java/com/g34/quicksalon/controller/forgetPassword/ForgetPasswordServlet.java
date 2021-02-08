855369aa5bdd6945fdeb64413e764facc9c7e37e
Shashira <shashiradw7@gmail.com>
2021-02-08 16:27:41 +0530

    login error fixed

---
+++ src/main/java/com/g34/quicksalon/controller/forgetPassword/ForgetPasswordServlet.java
@@ -0,0 +1,9 @@
+public class ForgetPasswordServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
