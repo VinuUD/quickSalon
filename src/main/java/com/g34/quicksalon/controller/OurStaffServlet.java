5eb3bcd6daaae676d5237713dc80736da9748e26
shehan82 <shehansandeepa82@gmail.com>
2021-02-10 11:48:35 +0530

    OurStaff servlet done

---
+++ src/main/java/com/g34/quicksalon/controller/OurStaffServlet.java
@@ -0,0 +1,39 @@
+package com.g34.quicksalon.controller;
+
+import com.g34.quicksalon.dao.OurStaffDAO;
+import com.g34.quicksalon.dao.OurStaffDAOImple;
+import com.g34.quicksalon.model.OurStaff;
+import com.google.gson.Gson;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class OurStaffServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse res ) throws IOException {
+
+        OurStaffDAO ourStaff = new OurStaffDAOImple();
+        try {
+            ArrayList<OurStaff> staffDetails = new ArrayList<>();
+            staffDetails = ourStaff.getStaffDetails();
+
+            String json = new Gson().toJson(staffDetails);
+            res.setContentType("application/json");
+            res.setCharacterEncoding("UTF-8");
+            res.getWriter().write(json);
+            String name = staffDetails.get(0).getFirstName();
+            System.out.println(name);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
