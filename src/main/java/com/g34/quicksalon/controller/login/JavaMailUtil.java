5054ee01ed58fd988e87f8cf0e44c28aaa57a612
Shashira <shashiradw7@gmail.com>
2021-02-12 18:35:16 +0530

    Mail body changed

--- src/main/java/com/g34/quicksalon/controller/login/JavaMailUtil.java
+++ src/main/java/com/g34/quicksalon/controller/login/JavaMailUtil.java
@@ -23,48 +23,17 @@ public class JavaMailUtil {
         Message message = new MimeMessage(session);
         message.setFrom(new InternetAddress("quicksalong34@gmail.com"));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-        message.setSubject("Quick Salon..");
+        message.setSubject("Quick Salon");
+
 
         MimeBodyPart mimeBodyPart = new MimeBodyPart();
-        mimeBodyPart.setContent(msg, "text/html");
+        mimeBodyPart.setContent("<h3>Password Reset</h3></br><b>"+msg+"</b>", "text/html");
         Multipart multipart = new MimeMultipart();
         multipart.addBodyPart(mimeBodyPart);
         message.setContent(multipart);
         Transport.send(message);
     }
 
-//    public void sendMail(String recepient) throws MessagingException {
-//
-//
-//        Properties properties=new Properties();
-//        properties.put("mail.smtp.auth","true");
-//        properties.put("mail.smtp.starttls.enable","true");
-//        properties.put("mail.smtp.host","smtp.gmail.com");
-//        properties.put("mail.smtp.port",587);
-//
-//
-//        Session session=Session.getInstance(properties, new Authenticator() {
-//            @Override
-//            protected PasswordAuthentication getPasswordAuthentication() {
-//                return new PasswordAuthentication(myemail,password);
-//            }
-//        });
-//        Message message=prepareMessage(session,myemail,recepient);
-//        try {
-//            Transport.send(message);
-//        } catch (MessagingException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    public Message prepareMessage(Session session,String myemail,String recipient) throws MessagingException {
-//        Message message=new MimeMessage(session);
-//            message.setFrom(new InternetAddress(myemail));
-//            message.setRecipient(Message.RecipientType.TO,new InternetAddress(recipient));
-//            message.setSubject("My first Mail");
-//            message.setText("Hi, Here You are PinCode : 112339");
-//        return message;
-//    }
+
 
 }
