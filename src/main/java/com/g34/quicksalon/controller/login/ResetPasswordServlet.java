aa83abe322090b14c3b3cdfac86ccba74d3cdf3f
Shashira <shashiradw7@gmail.com>
2021-02-12 18:33:18 +0530

    Reset password servlet added

---
+++ src/main/java/com/g34/quicksalon/controller/login/ResetPasswordServlet.java
@@ -0,0 +1,9 @@
+public class ResetPasswordServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
