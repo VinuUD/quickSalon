f68e48578be5a4ca47d4c1842b3f6cfb03f679d9
Shashira <shashiradw7@gmail.com>
2021-02-12 18:42:31 +0530

    verify email & update password methods created

--- src/main/java/com/g34/quicksalon/dao/UserDAO.java
+++ src/main/java/com/g34/quicksalon/dao/UserDAO.java
@@ -1,4 +1,10 @@
 package com.g34.quicksalon.dao;
 
+import java.sql.SQLException;
+
 public interface UserDAO {
+
+    public boolean isRegisteredUser(String email) throws SQLException, ClassNotFoundException;
+
+    public boolean resetPassword(String email,String password) throws SQLException, ClassNotFoundException;
 }
--- src/main/java/com/g34/quicksalon/dao/UserDAOImple.java
+++ src/main/java/com/g34/quicksalon/dao/UserDAOImple.java
@@ -1,4 +1,40 @@
 package com.g34.quicksalon.dao;
 
-public class UserDAOImple {
+import com.g34.quicksalon.database.DBConnection;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAOImple implements UserDAO{
+
+    @Override
+    public boolean isRegisteredUser(String email) throws SQLException, ClassNotFoundException {
+
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement("SELECT userID FROM j4f9qe_user WHERE email=?");
+        stmt.setString(1,email);
+
+        ResultSet resultSet = stmt.executeQuery();
+        if (resultSet.next()) {
+          return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public boolean resetPassword(String email,String password) throws SQLException, ClassNotFoundException {
+
+        PreparedStatement stmt = DBConnection.getConnection().prepareStatement("UPDATE j4f9qe_user SET password=? WHERE email=? ");
+        stmt.setString(1,password);
+        stmt.setString(2,email);
+
+        int reset=stmt.executeUpdate();
+        if (reset>0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
