496110441496bb32702f58eafc28f0f6b617757a
Shashira <shashiradw7@gmail.com>
2021-03-18 21:14:04 +0530

    EmployeeManagementDAO added

--- src/main/java/com/g34/quicksalon/dao/EmployeeManagementDAO.java
+++ src/main/java/com/g34/quicksalon/dao/EmployeeManagementDAO.java
@@ -1,2 +1,11 @@
-package com.g34.quicksalon.dao;public interface EmployeeManagementDAO {
+package com.g34.quicksalon.dao;
+
+import com.g34.quicksalon.model.Leave;
+
+import java.sql.SQLException;
+
+public interface EmployeeManagementDAO {
+
+    public boolean addLeave(Leave leave) throws SQLException, ClassNotFoundException;
+
 }
