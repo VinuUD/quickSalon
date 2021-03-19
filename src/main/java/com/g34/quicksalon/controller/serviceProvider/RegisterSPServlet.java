4dbf8758eab104b9fd81e6a0db7c708c914daa0b
Shashira <shashiradw7@gmail.com>
2021-03-19 15:17:23 +0530

    Created registerSPServlet

---
+++ src/main/java/com/g34/quicksalon/controller/serviceProvider/RegisterSPServlet.java
@@ -0,0 +1,71 @@
+package com.g34.quicksalon.controller.serviceProvider;
+
+import com.g34.quicksalon.dao.ServiceProviderDAO;
+import com.g34.quicksalon.dao.ServiceProviderDAOImple;
+import com.g34.quicksalon.model.ServiceProvider;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.SQLException;
+import java.util.Date;
+
+public class RegisterSPServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        int empID= Integer.parseInt(request.getParameter("empID"));
+        String fname=request.getParameter("fname");
+        String lname=request.getParameter("lname");
+        String uname=request.getParameter("uname");
+        String nic=request.getParameter("nic");
+        String email=request.getParameter("email");
+        String password=doHash(request.getParameter("password"));
+        float salary= Float.parseFloat(request.getParameter("salary"));
+
+        ServiceProvider serviceProvider=new ServiceProvider(empID,fname,lname,uname,nic,email,salary,password,new Date(),null,0,0,0,0);
+        ServiceProviderDAO serviceProviderDAO=new ServiceProviderDAOImple();
+        boolean success=false;
+        try {
+            success=serviceProviderDAO.registerServiceProvider(serviceProvider);
+            response.getWriter().println(success);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //        Return empId for newly added SP
+        ServiceProviderDAO serviceProviderDAO=new ServiceProviderDAOImple();
+        try {
+            int empID=serviceProviderDAO.getLastEmployeeID();
+            response.getWriter().println(empID);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String doHash(String password){
+        try {
+            MessageDigest messageDigest=MessageDigest.getInstance("SHA-256");
+            messageDigest.update(password.getBytes());
+            byte[] hashedByte=messageDigest.digest();
+            StringBuilder sb=new StringBuilder();
+            for(byte b:hashedByte){
+                sb.append(String.format("%02x",b));
+            }
+            return  sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
