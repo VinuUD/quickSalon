8bd70f67b389f5b2da95ca05411a64bd05194ec8
Shashira <shashiradw7@gmail.com>
2021-03-19 15:14:36 +0530

    Add a new constructor for register SP

--- src/main/java/com/g34/quicksalon/model/ServiceProvider.java
+++ src/main/java/com/g34/quicksalon/model/ServiceProvider.java
@@ -4,19 +4,23 @@ import java.util.Date;
 
 public class ServiceProvider {
     private int employeeId;
-    private String nicNo;
     private String firstName;
     private String lastName;
+    private String userName;
+    private String nicNo;
+    private String email;
     private float salary;
+    private String password;
+
     private Date enrollDate;
     private Date resignDate;
     private int isUpperStaffFlag;
     private int onLeaveFlag;
     private int removedFlag;
+    private int userID;
 
     public ServiceProvider() {
     }
-
 
     public ServiceProvider(int employeeId, String firstName, String lastName) {
         this.employeeId = employeeId;
@@ -24,11 +28,28 @@ public class ServiceProvider {
         this.lastName = lastName;
     }
 
-    public ServiceProvider(int employeeId, String nicNo, String firstName, String lastName, float salary, Date enrollDate, Date resignDate, int isUpperStaffFlag, int onLeaveFlag, int removedFlag) {
+    public ServiceProvider(int employeeId, String firstName, String lastName, String userName, String nicNo, String email, float salary, String password, Date enrollDate, Date resignDate, int isUpperStaffFlag, int onLeaveFlag, int removedFlag,int userID) {
         this.employeeId = employeeId;
-        this.nicNo = nicNo;
         this.firstName = firstName;
         this.lastName = lastName;
+        this.userName = userName;
+        this.nicNo = nicNo;
+        this.email = email;
+        this.salary = salary;
+        this.password = password;
+        this.enrollDate = enrollDate;
+        this.resignDate = resignDate;
+        this.isUpperStaffFlag = isUpperStaffFlag;
+        this.onLeaveFlag = onLeaveFlag;
+        this.removedFlag = removedFlag;
+        this.userID = userID;
+    }
+
+    public ServiceProvider(int employeeId, String nicNo, String firstName, String lastName, float salary, Date enrollDate, Date resignDate, int isUpperStaffFlag, int onLeaveFlag, int removedFlag) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nicNo = nicNo;
         this.salary = salary;
         this.enrollDate = enrollDate;
         this.resignDate = resignDate;
@@ -43,14 +64,6 @@ public class ServiceProvider {
 
     public void setEmployeeId(int employeeId) {
         this.employeeId = employeeId;
-    }
-
-    public String getNicNo() {
-        return nicNo;
-    }
-
-    public void setNicNo(String nicNo) {
-        this.nicNo = nicNo;
     }
 
     public String getFirstName() {
@@ -69,12 +82,44 @@ public class ServiceProvider {
         this.lastName = lastName;
     }
 
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getNicNo() {
+        return nicNo;
+    }
+
+    public void setNicNo(String nicNo) {
+        this.nicNo = nicNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public float getSalary() {
         return salary;
     }
 
     public void setSalary(float salary) {
         this.salary = salary;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Date getEnrollDate() {
@@ -115,5 +160,13 @@ public class ServiceProvider {
 
     public void setRemovedFlag(int removedFlag) {
         this.removedFlag = removedFlag;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
     }
 }
