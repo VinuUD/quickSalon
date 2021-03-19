4de16dfd6ee898626a63d363edc47d19b10e26c6
Shashira <shashiradw7@gmail.com>
2021-03-19 15:19:14 +0530

    Modified register SP page

---
+++ web/resources/javascript/register_service_provider.js
@@ -0,0 +1,53 @@
+$( document ).ready(function() {
+
+    $.get("http://localhost:8080/quickSalon_war_exploded/registerSP", function(data, status){
+        $('#empid').val(data);
+    });
+
+
+    $('#register').on('click', function(){
+
+        if ($('#fname').val()=='') {
+            $('#fname').css("border", "4px red solid");
+        }
+        if ($('#lname').val() == '') {
+            $('#lname').css("border", "4px red solid");
+        } else if ($('#uname').val() == '') {
+            $('#uname').css("border", "4px red solid");
+        } else if ($('#nic').val() == '') {
+            $('#nic').css("border", "4px red solid");
+        } else if ($('#email').val() == '') {
+            $('#email').css("border", "4px red solid");
+
+        }else if ($('#salaryl').val() == '') {
+            $('#salary').css("border", "4px red solid");
+        }
+        else if ($('#password').val() == '') {
+            $('#password').css("border", "4px red solid");
+        }
+         else {
+            //On success leave added
+            confirm("Do you want to add Service Provider?");
+
+            $.post("http://localhost:8080/quickSalon_war_exploded/registerSP", {
+                    empID: $("#empid").val(),
+                    fname: $("#fname").val(),
+                    lname: $("#lname").val(),
+                    uname: $("#uname").val(),
+                    nic: $("#nic").val(),
+                    email: $("#email").val(),
+                    salary: $("#salary").val(),
+                    password: $("#password").val()
+                },
+                function (data, status) {
+                    location.reload();
+                }
+            );
+        }
+    });
+
+
+
+});
+
+
