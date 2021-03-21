ae6170e2df49026d34afd0c9250af8ebd3c22051
Shashira <shashiradw7@gmail.com>
2021-03-21 18:05:00 +0530

    Add function to get Appoiments by SP

--- src/main/java/com/g34/quicksalon/dao/ServiceProviderDAOImple.java
+++ src/main/java/com/g34/quicksalon/dao/ServiceProviderDAOImple.java
@@ -1,6 +1,7 @@
 package com.g34.quicksalon.dao;
 
 import com.g34.quicksalon.database.DBConnection;
+import com.g34.quicksalon.model.Appointment;
 import com.g34.quicksalon.model.CustomerDetails;
 import com.g34.quicksalon.model.ServiceProvider;
 
@@ -10,8 +11,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ServiceProviderDAOImple implements  ServiceProviderDAO{
-
-
 
     //get sps by serviceIDs
     public ArrayList<ServiceProvider> getServiceProvidersByID(int sid) {
@@ -34,7 +33,7 @@ public class ServiceProviderDAOImple implements  ServiceProviderDAO{
         return serviceProviders;
     }
 
-//    This will return the id->full name pair of sps whose provide given serviceId
+    //    This will return the id->full name pair of sps whose provide given serviceId
     public HashMap<String,String> getSPList(int serviceId) {
         HashMap<String, String> spList = new HashMap<String, String>();
         try {
@@ -119,6 +118,43 @@ public class ServiceProviderDAOImple implements  ServiceProviderDAO{
             connection.rollback();
             return false;
         }
+    }
+
+    @Override
+    public boolean assignServiceToSP(int serviceID, int spID) throws SQLException, ClassNotFoundException {
+        Connection connection =DBConnection.getConnection();
+        PreparedStatement stmt= connection.prepareStatement("INSERT INTO j4f9qe_servicesprovided VALUES (?,?)");
+        stmt.setInt(1,serviceID);
+        stmt.setInt(2,spID);
+
+        int success=stmt.executeUpdate();
+        if(success>0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //     get all upcoming appointment by SP -- (QId,date,startTime,endTime)
+    @Override
+    public ArrayList<Appointment> getAppointmentDeatilsBySP(int empID) throws SQLException, ClassNotFoundException {
+
+        ArrayList<Appointment> upAppointments=new ArrayList<>();
+
+        try {
+            PreparedStatement stmt=DBConnection.getConnection().prepareStatement("SELECT qID,date,startTime,endTime FROM j4f9qe_spappointmentsdetails WHERE date>=NOW() AND cancelledFlag!=1 AND employeeID=(?);");
+            stmt.setInt(1,empID);
+            ResultSet resultSet = stmt.executeQuery();
+
+            while (resultSet.next()){
+                Appointment appointment=new Appointment(resultSet.getInt(1),resultSet.getDate(2),resultSet.getTime(3),resultSet.getTime(4));
+                upAppointments.add(appointment);
+            }
+
+        } catch (SQLException | ClassNotFoundException throwables) {
+            throwables.printStackTrace();
+        }
+        return upAppointments;
     }
 
 
