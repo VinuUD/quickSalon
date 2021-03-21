5c4dd4d504d46ba3acddce2397446b11e508b2a3
Shashira <shashiradw7@gmail.com>
2021-03-21 18:08:53 +0530

    Craeted a package service MGT

---
+++ src/main/java/com/g34/quicksalon/controller/serviceManagement/ServiceListServlet.java
@@ -0,0 +1,34 @@
+package com.g34.quicksalon.controller.ServiceManagement;
+
+import com.g34.quicksalon.dao.ServiceDAO;
+import com.g34.quicksalon.dao.ServiceDAOImple;
+import com.g34.quicksalon.model.Service;
+import com.google.gson.Gson;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServiceListServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        try {
+             ServiceDAO serviceDAO=new ServiceDAOImple();
+             ArrayList<Service> services=new ArrayList<>();
+             services= serviceDAO.getAllServiceNames();
+             String json = new Gson().toJson(services);
+             response.setContentType("application/json");
+             response.setCharacterEncoding("UTF-8");
+             response.getWriter().write(json);
+
+         }catch (Exception e){
+             response.getWriter().println(e.getMessage());
+         }
+    }
+}
