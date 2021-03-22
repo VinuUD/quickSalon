75b8ea654032c7429e6f1410c3e6f74e12aa690c
shehan82 <shehansandeepa82@gmail.com>
2021-03-22 13:06:03 +0530

    css, js, ui changes

--- web/resources/javascript/addLeave.js
+++ web/resources/javascript/addLeave.js
@@ -1,41 +1,31 @@
 var i = 0;
 
-function hello()
-{
-    i = i + 1;
-    return i;
+function hello() {
+  i = i + 1;
+  return i;
 }
 
-function change()
-{
-    if(i==0)
-    {
-        return 0;
-    }
-
-    else
-    {
-        document.getElementById("mainPopupDiv").style.display="block";
-        i=0;
-    }
+function change() {
+  if (i == 0) {
+    return 0;
+  } else {
+    document.getElementById("mainPopupDiv").style.display = "block";
+    i = 0;
+  }
 }
 
-function hide()
-{
-    document.getElementById("mainPopupDiv").style.display="none";
+function hide() {
+  document.getElementById("mainPopupDiv").style.display = "none";
 }
 
-function show()
-{
-    document.getElementById("mainPopupDivCal").style.display="block";
+function show() {
+  document.getElementById("mainPopupDivCal").style.display = "block";
 }
 
-function showSchedule()
-{
-    document.getElementById("mainPopupDivSchedule").style.display="block";
+function showSchedule() {
+  document.getElementById("mainPopupDivSchedule").style.display = "block";
 }
 
-function hideSchedule()
-{
-    document.getElementById("mainPopupDivSchedule").style.display="none";
+function hideSchedule() {
+  document.getElementById("mainPopupDivSchedule").style.display = "none";
 }
