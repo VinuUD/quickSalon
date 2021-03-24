1cf76daaac463ef41c949a4d81875950d2e53af9
Shashira <shashiradw7@gmail.com>
2021-03-25 05:16:56 +0530

    Calendar data loaded into Saon schedule

---
+++ web/resources/javascript/salon_schedule.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+
+     //    Get all Appointments and load to calendar
+   $.get("http://localhost:8080/quickSalon_war_exploded/appointments",
+        function (responseJson) {
+            $.each(responseJson, function (index, appointment) {
+                //set date into 2020Jan05 format
+                res = appointment.date.replace(",", "").split(" ");
+                day = res[1] < 10 ? "0" + res[1] : res[1];
+                selectId = res[2] + res[0] + day;
+                // alert(selectId)
+                $("#" + selectId).css("background-color", "#F58F79");
+            });
+        }
+    );
+
+    //Requst for load Free time slots
+
+    
+
+})
